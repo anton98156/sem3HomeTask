3044edbead373bbe07b5068d66bc8d4e7b51a500
Антон Кравцов <antonkravcov@MacBook-Air-Anton.local>
2024-01-20 19:07:57 +0300

    UserService updated

--- src/main/java/com/example/sem3HomeTask/services/UserService.java
+++ src/main/java/com/example/sem3HomeTask/services/UserService.java
@@ -1,13 +1,11 @@
 package com.example.sem3HomeTask.services;
 
 import com.example.sem3HomeTask.domain.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-   // @Autowired
     private NotificationService notificationService;
 
     // Внедрение зависимости через конструктор
