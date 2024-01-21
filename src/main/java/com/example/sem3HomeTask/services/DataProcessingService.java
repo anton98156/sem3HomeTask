a796f11643608361367713ff53f0ec898211cc2e
Антон Кравцов <antonkravcov@MacBook-Air-Anton.local>
2024-01-21 12:15:35 +0300

    Added process registration

--- src/main/java/com/example/sem3HomeTask/services/DataProcessingService.java
+++ src/main/java/com/example/sem3HomeTask/services/DataProcessingService.java
@@ -39,8 +39,7 @@ public class DataProcessingService {
                 .orElse(0);
     }
 
-    public void  addUserToList(User user)
-    {
+    public void addUserToList(User user) {
         repository.getUsers().add(user);
     }
 }
--- src/main/java/com/example/sem3HomeTask/services/RegistrationService.java
+++ src/main/java/com/example/sem3HomeTask/services/RegistrationService.java
@@ -3,18 +3,28 @@ package com.example.sem3HomeTask.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.sem3HomeTask.domain.User;
+
 @Service
 public class RegistrationService {
 
+    @Autowired
+    private DataProcessingService dataProcessingService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private NotificationService notificationService;
 
     public DataProcessingService getDataProcessingService() {
         return dataProcessingService;
     }
 
-    @Autowired
-    private DataProcessingService dataProcessingService;
+    public void processRegistration(String name, int age, String email) {
+        User user = userService.createUser(name, age, email);
+        dataProcessingService.addUserToList(user);
+        notificationService.sendNotification("");
+    }
 
-    //Поля UserService, NotificationService
-
-    //Метод processRegistration
 }
